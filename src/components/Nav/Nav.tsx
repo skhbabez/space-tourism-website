4ead0bc1d9e959b7d38402dde828532415de281d
Sascha Khbabez <sascha.khbabez@outlook.de>
2026-01-26 22:29:36 +0100

    finished navigation added tablet and desktop styles

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useMatchRoute } from "@tanstack/react-router";
 import clsx from "clsx";
 import { useId, type ComponentPropsWithRef } from "react";
 import { FaBars } from "react-icons/fa6";
@@ -12,15 +12,18 @@ const links = [
   { to: "/technology", name: "technology" },
 ];
 
-const selected = true;
 const Nav = ({
   className,
   ...props
 }: Omit<ComponentPropsWithRef<"nav">, "children">) => {
-  const id = useId();
+  const matchRoute = useMatchRoute();
+  const popoverId = useId();
   return (
     <nav
-      className={clsx("p-6 flex justify-between w-full", className)}
+      className={clsx(
+        "p-6 md:p-0 flex justify-between items-center w-full font-barlow-condensed",
+        className,
+      )}
       {...props}
     >
       <img
@@ -28,23 +31,54 @@ const Nav = ({
         height={40}
         src={logo}
         alt=""
-        className="aspect-square w-10 md:w-12"
+        className="aspect-square w-10 md:w-12 md:mx-10 xl:mx-16"
       />
-      <button popoverTarget={id} popoverTargetAction="show">
+      <ol
+        start={0}
+        className="nav-list-style hidden md:flex justify-end gap-12 pe-10 xl:pe-16 backdrop-blur-xs bg-white/5 max-w-184 w-full"
+      >
+        {links.map(({ to, name }) => (
+          <li className="relative py-[38.5px]" key={name}>
+            <Link
+              className="ps-3 uppercase text-8 text-white tracking-[2px] peer"
+              to={to}
+            >
+              {name}
+            </Link>
+            <span
+              className={clsx(
+                "hidden absolute left-0 bottom-0 w-full h-0.75 bg-white",
+                matchRoute({ to: to, fuzzy: true })
+                  ? "inline"
+                  : "peer-hover:inline peer-hover:opacity-50",
+              )}
+            ></span>
+          </li>
+        ))}
+      </ol>
+      <button
+        className="md:hidden"
+        popoverTarget={popoverId}
+        popoverTargetAction="show"
+      >
         <FaBars size={28} className="text-blue-300" />
       </button>
       <div
         popover="auto"
-        id={id}
+        id={popoverId}
         className={clsx(
-          "bg-blue-900 left-auto fixed right-0 mx-0 backdrop-blur-xs ps-8 h-screen min-w-63.5 font-barlow-condensed space-y-12",
+          "md:hidden bg-blue-900/15 left-auto fixed right-0 mx-0 backdrop-blur-xs ps-8 h-screen min-w-63.5  space-y-12",
           "starting:[&:popover-open]:translate-x-full [&:popover-open]:translate-x-0 translate-x-full",
           "motion-reduce:transition-none transition-discrete transition-[display,translate] duration-300 ease-in-out",
         )}
       >
         <div>
           <div className="py-8 px-6 text-white text-end">
-            <button type="button" popoverTarget={id} popoverTargetAction="hide">
+            <button
+              type="button"
+              popoverTarget={popoverId}
+              popoverTargetAction="hide"
+            >
               <FaX size={25} className="text-blue-300" />
             </button>
           </div>
@@ -60,7 +94,7 @@ const Nav = ({
                 <span
                   className={clsx(
                     "hidden absolute right-0 h-full w-0.75 bg-white",
-                    selected
+                    matchRoute({ to: to, fuzzy: true })
                       ? "inline"
                       : "peer-hover:inline peer-hover:opacity-50",
                   )}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Nav } from "../components/Nav/Nav";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,6 +9,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
+      <Nav />
       <Outlet />
       <TanStackRouterDevtools />
     </>
