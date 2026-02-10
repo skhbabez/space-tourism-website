ceafd93f66ab6f7b78b15d913359a5eab31cce8e
Sascha Khbabez <sascha.khbabez@outlook.de>
2026-02-10 22:50:11 +0100

    fixed spacing

--- src/pages/Destination/DestinationPage.tsx
+++ src/pages/Destination/DestinationPage.tsx
@@ -13,7 +13,7 @@ const DestinationPage = ({
 
   return (
     <div className={clsx("p-6 md:p-10 xl:p-12 h-full", className)} {...props}>
-      <section className="max-w-277.5 mx-auto space-y-6 md:max-w-172 xl:max-w-277.5 h-full ">
+      <section className="max-w-277.5 mx-auto space-y-6 md:max-w-172 xl:max-w-277.5 h-full flex flex-col">
         <h1 className="text-6-mobile md:text-5-tablet xl:text-5-desktop font-barlow-condensed uppercase text-white text-center md:text-start">
           <span className="font-bold text-white/25 pe-6">01</span>pick your
           destination
